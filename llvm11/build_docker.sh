c161914b49c89a3153669a41470f49be54a9c3c1
toshihiro konda <kontoshi0317@gmail.com>
2021-04-18 16:31:58 +0900

    update for llvm11

--- llvm11/build_docker.sh
+++ llvm11/build_docker.sh
@@ -1,1 +1,1 @@
-docker build -t llvm11-centos6:1.3 . --build-arg USER=test --build-arg PASSWD=test
+docker run -it --rm llvm10-centos6:1.2
--- llvm11/run_docker.sh
+++ llvm11/run_docker.sh
@@ -1,1 +1,1 @@
-docker run -it --rm llvm11-centos6:1.3
+docker run -it --rm llvm11-centos6:1.4
